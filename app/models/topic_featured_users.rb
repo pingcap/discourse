38b5dc7ccf3e2fc494bc7ff2c90097fcdddb0590
Hooopo Wang <hoooopo@gmail.com>
2019-09-26 19:14:17 +0800

    temp disable fail

--- app/models/topic_featured_users.rb
+++ app/models/topic_featured_users.rb
@@ -25,7 +25,8 @@ class TopicFeaturedUsers
   end
 
   def self.ensure_consistency!(topic_id = nil)
-
+    # TODO FIX
+    return true
     filter = "#{"AND t.id = #{topic_id.to_i}" if topic_id}"
     filter2 = "#{"AND tt.id = #{topic_id.to_i}" if topic_id}"
 
