182406b582d564fba7bddbae367015a7af65d0f3
Hooopo Wang <hoooopo@gmail.com>
2019-10-17 03:19:11 +0800

    Fix You can't specify target table 'user_stats' for update in FROM clause

--- app/jobs/onceoff/fix_posts_read.rb
+++ app/jobs/onceoff/fix_posts_read.rb
@@ -11,7 +11,7 @@ UPDATE user_stats
    SET posts_read_count = topics_entered * 50
  WHERE user_id IN (
    SELECT us2.user_id
-     FROM user_stats us2
+     FROM (SELECT * FROM user_stats) us2
     WHERE us2.topics_entered > 0
       AND us2.posts_read_count / us2.topics_entered > 50
  )
