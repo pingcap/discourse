a41293b58ffb318fe780ad48043e489d379bf33b
Hooopo Wang <hoooopo@gmail.com>
2019-10-19 13:59:10 +0800

    fix UPDATE & DELETE

--- app/models/directory_item.rb
+++ app/models/directory_item.rb
@@ -44,12 +44,12 @@ class DirectoryItem < ActiveRecord::Base
 
     ActiveRecord::Base.transaction do
       # Delete records that belonged to users who have been deleted
-      DB.exec "DELETE FROM directory_items
-                USING directory_items di
+      DB.exec "DELETE directory_items
+                 FROM directory_items
                 LEFT JOIN users u ON (u.id = user_id AND u.active AND u.silenced_till IS NULL AND u.id > 0)
-                WHERE di.id = directory_items.id AND
+                WHERE
                       u.id IS NULL AND
-                      di.period_type = :period_type", period_type: period_types[period_type]
+                      directory_items.period_type = :period_type", period_type: period_types[period_type]
 
       # Create new records for users who don't have one yet
       DB.exec "INSERT INTO directory_items(period_type, user_id, likes_received, likes_given, topics_entered, days_visited, posts_read, topic_count, post_count)
@@ -74,36 +74,38 @@ class DirectoryItem < ActiveRecord::Base
       # TODO
       # WARNING: post_count is a wrong name, it should be reply_count (excluding topic post)
       #
-      DB.exec "WITH x AS (SELECT
-                    u.id user_id,
-                    SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :was_liked_type THEN 1 ELSE 0 END) likes_received,
-                    SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :like_type THEN 1 ELSE 0 END) likes_given,
-                    COALESCE((SELECT COUNT(topic_id) FROM topic_views AS v WHERE v.user_id = u.id AND v.viewed_at > :since), 0) topics_entered,
-                    COALESCE((SELECT COUNT(id) FROM user_visits AS uv WHERE uv.user_id = u.id AND uv.visited_at > :since), 0) days_visited,
-                    COALESCE((SELECT SUM(posts_read) FROM user_visits AS uv2 WHERE uv2.user_id = u.id AND uv2.visited_at > :since), 0) posts_read,
-                    SUM(CASE WHEN t2.id IS NOT NULL AND ua.action_type = :new_topic_type THEN 1 ELSE 0 END) topic_count,
-                    SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :reply_type THEN 1 ELSE 0 END) post_count
-                  FROM users AS u
-                  LEFT OUTER JOIN user_actions AS ua ON ua.user_id = u.id AND COALESCE(ua.created_at, :since) > :since
-                  LEFT OUTER JOIN posts AS p ON ua.target_post_id = p.id AND p.deleted_at IS NULL AND p.post_type = :regular_post_type AND NOT p.hidden
-                  LEFT OUTER JOIN topics AS t ON p.topic_id = t.id AND t.archetype = 'regular' AND t.deleted_at IS NULL AND t.visible
-                  LEFT OUTER JOIN topics AS t2 ON t2.id = ua.target_topic_id AND t2.archetype = 'regular' AND t2.deleted_at IS NULL AND t2.visible
-                  LEFT OUTER JOIN categories AS c ON t.category_id = c.id
-                  WHERE u.active
-                    AND u.silenced_till IS NULL
-                    AND u.id > 0
-                  GROUP BY u.id)
-      UPDATE directory_items di SET
-               likes_received = x.likes_received,
-               likes_given = x.likes_given,
-               topics_entered = x.topics_entered,
-               days_visited = x.days_visited,
-               posts_read = x.posts_read,
-               topic_count = x.topic_count,
-               post_count = x.post_count
-      FROM x
+      DB.exec "
+      UPDATE directory_items di 
+      INNER JOIN (
+        SELECT
+          u.id user_id,
+          SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :was_liked_type THEN 1 ELSE 0 END) likes_received,
+          SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :like_type THEN 1 ELSE 0 END) likes_given,
+          COALESCE((SELECT COUNT(topic_id) FROM topic_views AS v WHERE v.user_id = u.id AND v.viewed_at > :since), 0) topics_entered,
+          COALESCE((SELECT COUNT(id) FROM user_visits AS uv WHERE uv.user_id = u.id AND uv.visited_at > :since), 0) days_visited,
+          COALESCE((SELECT SUM(posts_read) FROM user_visits AS uv2 WHERE uv2.user_id = u.id AND uv2.visited_at > :since), 0) posts_read,
+          SUM(CASE WHEN t2.id IS NOT NULL AND ua.action_type = :new_topic_type THEN 1 ELSE 0 END) topic_count,
+          SUM(CASE WHEN p.id IS NOT NULL AND t.id IS NOT NULL AND ua.action_type = :reply_type THEN 1 ELSE 0 END) post_count
+        FROM users AS u
+        LEFT OUTER JOIN user_actions AS ua ON ua.user_id = u.id AND COALESCE(ua.created_at, :since) > :since
+        LEFT OUTER JOIN posts AS p ON ua.target_post_id = p.id AND p.deleted_at IS NULL AND p.post_type = :regular_post_type AND NOT p.hidden
+        LEFT OUTER JOIN topics AS t ON p.topic_id = t.id AND t.archetype = 'regular' AND t.deleted_at IS NULL AND t.visible
+        LEFT OUTER JOIN topics AS t2 ON t2.id = ua.target_topic_id AND t2.archetype = 'regular' AND t2.deleted_at IS NULL AND t2.visible
+        LEFT OUTER JOIN categories AS c ON t.category_id = c.id
+        WHERE u.active
+          AND u.silenced_till IS NULL
+          AND u.id > 0
+        GROUP BY u.id
+      ) x ON x.user_id = di.user_id
+         SET
+           di.likes_received = x.likes_received,
+           di.likes_given = x.likes_given,
+           di.topics_entered = x.topics_entered,
+           di.days_visited = x.days_visited,
+           di.posts_read = x.posts_read,
+           di.topic_count = x.topic_count,
+           di.post_count = x.post_count
       WHERE
-        x.user_id = di.user_id AND
         di.period_type = :period_type AND (
         di.likes_received <> x.likes_received OR
         di.likes_given <> x.likes_given OR
@@ -125,14 +127,12 @@ class DirectoryItem < ActiveRecord::Base
       if period_type == :all
         DB.exec <<~SQL
           UPDATE user_stats s
-          SET likes_given         = d.likes_given,
-              likes_received      = d.likes_received,
-              topic_count         = d.topic_count,
-              post_count          = d.post_count
-
-          FROM directory_items d
-          WHERE s.user_id = d.user_id AND
-                d.period_type = 1 AND
+          INNER JOIN directory_items d ON s.user_id = d.user_id
+          SET s.likes_given         = d.likes_given,
+              s.likes_received      = d.likes_received,
+              s.topic_count         = d.topic_count,
+              s.post_count          = d.post_count
+          WHERE d.period_type = 1 AND
             ( s.likes_given         <> d.likes_given OR
               s.likes_received      <> d.likes_received OR
               s.topic_count         <> d.topic_count OR
