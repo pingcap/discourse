d8aa65778ac2a0f647e32d4061d6af781fd52001
Hooopo Wang <hoooopo@gmail.com>
2021-10-12 01:23:41 +0800

    fix RemoveSuppressFromLatestFromCategory

--- db/migrate/20191107190330_remove_suppress_from_latest_from_category.rb
+++ db/migrate/20191107190330_remove_suppress_from_latest_from_category.rb
@@ -6,36 +6,34 @@ class RemoveSuppressFromLatestFromCategory < ActiveRecord::Migration[6.0]
   }
 
   def up
-    # TODO FIX
-    if false
-      ids = DB.query_single("SELECT id::text FROM categories WHERE suppress_from_latest = TRUE")
+    ids = DB.query_single("SELECT id FROM categories WHERE suppress_from_latest = TRUE")
 
-      if ids.present?
-        muted_ids = DB.query_single("SELECT value from site_settings WHERE name = 'default_categories_muted'").first
-        ids += muted_ids.split("|") if muted_ids.present?
-        ids.uniq!
+    if ids.present?
+      ids.map!{|x| x.to_s}
+      muted_ids = DB.query_single("SELECT value from site_settings WHERE name = 'default_categories_muted'").first
+      ids += muted_ids.split("|") if muted_ids.present?
+      ids.uniq!
 
-        # We shouldn't encourage to have more than 10 categories in `default_categories_muted` site setting.
-        if ids.count <= 10
-          # CategoryUser.notification_levels[:muted] is 0, avoid reaching to object model
-          DB.exec(<<~SQL, muted: 0)
-            INSERT IGNORE INTO category_users (category_id, user_id, notification_level)
-              SELECT c.id category_id, u.id user_id, :muted
-              FROM users u
-                CROSS JOIN categories c
-                LEFT JOIN category_users cu
-                  ON u.id = cu.user_id
-                    AND c.id = cu.category_id
-              WHERE c.suppress_from_latest = TRUE
-                AND cu.notification_level IS NULL
-          SQL
+      # We shouldn't encourage to have more than 10 categories in `default_categories_muted` site setting.
+      if ids.count <= 10
+        # CategoryUser.notification_levels[:muted] is 0, avoid reaching to object model
+        DB.exec(<<~SQL, muted: 0)
+          INSERT IGNORE INTO category_users (category_id, user_id, notification_level)
+            SELECT c.id category_id, u.id user_id, :muted
+            FROM users u
+              CROSS JOIN categories c
+              LEFT JOIN category_users cu
+                ON u.id = cu.user_id
+                  AND c.id = cu.category_id
+            WHERE c.suppress_from_latest = TRUE
+              AND cu.notification_level IS NULL
+        SQL
 
-          DB.exec(<<~SQL, value: ids.join("|"))
-            UPDATE site_settings
-            SET value = :value
-            WHERE name = 'default_categories_muted'
-          SQL
-        end
+        DB.exec(<<~SQL, value: ids.join("|"))
+          UPDATE site_settings
+          SET value = :value
+          WHERE name = 'default_categories_muted'
+        SQL
       end
     end
 
