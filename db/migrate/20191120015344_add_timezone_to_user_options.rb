966ba61f94a6b64415394af2b594ae577afb1f77
Hooopo Wang <hoooopo@gmail.com>
2021-10-14 02:58:22 +0800

    fix update user options

--- db/migrate/20191120015344_add_timezone_to_user_options.rb
+++ db/migrate/20191120015344_add_timezone_to_user_options.rb
@@ -3,15 +3,13 @@
 class AddTimezoneToUserOptions < ActiveRecord::Migration[6.0]
   def up
     add_column :user_options, :timezone, :string
-    # TODO FIX
-    # execute(
-    #   <<-SQL
-    #     UPDATE user_options
-    #     SET timezone = ucf.value
-    #     FROM user_custom_fields AS ucf
-    #     WHERE ucf.user_id = user_options.user_id AND ucf.name = 'timezone'
-    #   SQL
-    # )
+    execute(
+      <<-SQL
+        UPDATE user_options inner join user_custom_fields as ucf
+        SET timezone = ucf.value
+        WHERE ucf.user_id = user_options.user_id AND ucf.name = 'timezone'
+      SQL
+    )
   end
 
   def down
