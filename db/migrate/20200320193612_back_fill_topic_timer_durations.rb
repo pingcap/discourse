f1094f38ab59e22ad026f0d0130e5b9d91f6916e
Hooopo Wang <hoooopo@gmail.com>
2021-10-14 19:00:50 +0800

    fix update topic_timers

--- db/migrate/20200320193612_back_fill_topic_timer_durations.rb
+++ db/migrate/20200320193612_back_fill_topic_timer_durations.rb
@@ -2,18 +2,17 @@
 
 class BackFillTopicTimerDurations < ActiveRecord::Migration[6.0]
   def up
-    # TODO FIX
-    # DB.exec <<~SQL
-    #   UPDATE topic_timers
-    #   SET duration = 0
-    #   WHERE duration IS NULL AND (execute_at IS NULL OR created_at IS NULL)
-    # SQL
+    DB.exec <<~SQL
+      UPDATE topic_timers
+      SET duration = 0
+      WHERE duration IS NULL AND (execute_at IS NULL OR created_at IS NULL)
+    SQL
 
-    # DB.exec <<~SQL
-    #   UPDATE topic_timers
-    #   SET duration = EXTRACT(EPOCH FROM execute_at - created_at) / 3600
-    #   WHERE duration IS NULL AND execute_at IS NOT NULL AND created_at IS NOT NULL
-    # SQL
+    DB.exec <<~SQL
+      UPDATE topic_timers
+      SET duration = (UNIX_TIMESTAMP(execute_at) - UNIX_TIMESTAMP(created_at)) / 3600
+      WHERE duration IS NULL AND execute_at IS NOT NULL AND created_at IS NOT NULL
+    SQL
   end
 
   def down
