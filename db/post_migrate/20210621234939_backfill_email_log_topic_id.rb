c36a536297bfd6f063c6272f788e24a8e920ecd1
Hooopo Wang <hoooopo@gmail.com>
2021-10-15 05:01:36 +0800

    fix update email_logs

--- db/post_migrate/20210621234939_backfill_email_log_topic_id.rb
+++ db/post_migrate/20210621234939_backfill_email_log_topic_id.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 class BackfillEmailLogTopicId < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
   BATCH_SIZE = 30_000
 
   def up
-    return # TODO FIX
     loop do
       count = DB.exec(<<~SQL, batch_size: BATCH_SIZE)
           WITH cte AS (
@@ -16,8 +14,8 @@ class BackfillEmailLogTopicId < ActiveRecord::Migration[6.1]
             LIMIT :batch_size
           )
           UPDATE email_logs
-          SET topic_id = cte.topic_id
-          FROM cte
+          INNER JOIN cte  ON email_logs.id = cte.id
+          SET email_logs.topic_id = cte.topic_id
           WHERE email_logs.id = cte.id
       SQL
 
