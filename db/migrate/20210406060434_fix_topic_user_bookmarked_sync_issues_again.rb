e458be412b0e7a0ed971c48ee20db205147c8e4b
Hooopo Wang <hoooopo@gmail.com>
2021-10-15 05:28:15 +0800

    fix topic_users.bookmarked

--- db/migrate/20210406060434_fix_topic_user_bookmarked_sync_issues_again.rb
+++ db/migrate/20210406060434_fix_topic_user_bookmarked_sync_issues_again.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 class FixTopicUserBookmarkedSyncIssuesAgain < ActiveRecord::Migration[6.0]
-  disable_ddl_transaction!
 
   def up
-    # TODO FIX
-    # DB.exec(
-    #   <<~SQL
-    #     UPDATE topic_users SET bookmarked = true
-    #     FROM bookmarks AS b
-    #     WHERE NOT topic_users.bookmarked AND topic_users.topic_id = b.topic_id AND topic_users.user_id = b.user_id
-    #   SQL
-    # )
+    DB.exec(
+      <<~SQL
+        UPDATE topic_users 
+        INNER JOIN bookmarks AS b ON topic_users.topic_id = b.topic_id AND topic_users.user_id = b.user_id
+        SET bookmarked = true
+        WHERE NOT topic_users.bookmarked AND topic_users.topic_id = b.topic_id AND topic_users.user_id = b.user_id
+      SQL
+    )
 
-    # DB.exec(
-    #   <<~SQL
-    #     UPDATE topic_users SET bookmarked = false
-    #     WHERE topic_users.bookmarked AND (SELECT COUNT(*) FROM bookmarks WHERE topic_id = topic_users.topic_id AND user_id = topic_users.user_id) = 0
-    #   SQL
-    # )
+    DB.exec(
+      <<~SQL
+        UPDATE topic_users 
+        SET bookmarked = false
+        WHERE topic_users.bookmarked AND (SELECT COUNT(*) FROM bookmarks WHERE topic_id = topic_users.topic_id AND user_id = topic_users.user_id) = 0
+      SQL
+    )
   end
 
   def down
