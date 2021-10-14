0f94e7658373809ec095010bdb8bf41d3d378b03
Hooopo Wang <hoooopo@gmail.com>
2021-10-14 15:15:14 +0800

    fix update user_stats

--- db/migrate/20191220134101_add_distinct_badge_count_to_user_stats.rb
+++ db/migrate/20191220134101_add_distinct_badge_count_to_user_stats.rb
@@ -2,18 +2,17 @@
 class AddDistinctBadgeCountToUserStats < ActiveRecord::Migration[6.0]
   def change
     add_column :user_stats, :distinct_badge_count, :integer, default: 0, null: false
-    # TODO FIX
-    # execute <<~SQL
-    #   UPDATE user_stats
-    #   SET distinct_badge_count = x.distinct_badge_count
-    #   FROM (
-    #     SELECT users.id user_id, COUNT(distinct user_badges.badge_id) distinct_badge_count
-    #     FROM users
-    #     LEFT JOIN user_badges ON user_badges.user_id = users.id
-    #                           AND (user_badges.badge_id IN (SELECT id FROM badges WHERE enabled))
-    #     GROUP BY users.id
-    #   ) x
-    #   WHERE user_stats.user_id = x.user_id AND user_stats.distinct_badge_count <> x.distinct_badge_count
-    # SQL
+    execute <<~SQL
+      UPDATE user_stats
+      INNER JOIN (
+        SELECT users.id user_id, COUNT(distinct user_badges.badge_id) distinct_badge_count
+        FROM users
+        LEFT JOIN user_badges ON 1=1 WHERE user_badges.user_id = users.id
+                              AND (user_badges.badge_id IN (SELECT id FROM badges WHERE enabled))
+        GROUP BY users.id
+      ) x
+      SET user_stats.distinct_badge_count = x.distinct_badge_count
+      WHERE user_stats.user_id = x.user_id AND user_stats.distinct_badge_count <> x.distinct_badge_count
+    SQL
   end
 end
