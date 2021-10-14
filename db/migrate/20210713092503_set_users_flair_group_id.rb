2c3248dff9f216b50884c7dbb78b7c50418e39c5
Hooopo Wang <hoooopo@gmail.com>
2021-10-15 05:08:42 +0800

    fix update users.flair_group_id

--- db/migrate/20210713092503_set_users_flair_group_id.rb
+++ db/migrate/20210713092503_set_users_flair_group_id.rb
@@ -2,14 +2,13 @@
 
 class SetUsersFlairGroupId < ActiveRecord::Migration[6.1]
   def change
-    # TODO FIX
-    # execute <<~SQL
-    #   UPDATE users
-    #   SET flair_group_id = primary_group_id
-    #   FROM groups
-    #   WHERE users.primary_group_id = groups.id AND
-    #         users.flair_group_id IS NULL AND
-    #         (groups.flair_icon IS NOT NULL OR groups.flair_upload_id IS NOT NULL)
-    # SQL
+    execute <<~SQL
+      UPDATE users
+      INNER JOIN `groups` ON users.primary_group_id = groups.id
+      SET flair_group_id = primary_group_id
+      WHERE users.primary_group_id = `groups`.id AND
+            users.flair_group_id IS NULL AND
+            (`groups`.flair_icon IS NOT NULL OR `groups`.flair_upload_id IS NOT NULL)
+    SQL
   end
 end
