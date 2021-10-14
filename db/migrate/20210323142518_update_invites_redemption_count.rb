9fb7c41811d7efa3d2b482f48898ecd2e587a871
Hooopo Wang <hoooopo@gmail.com>
2021-10-15 05:29:55 +0800

    fix invites.redemption_count

--- db/migrate/20210323142518_update_invites_redemption_count.rb
+++ db/migrate/20210323142518_update_invites_redemption_count.rb
@@ -2,17 +2,16 @@
 
 class UpdateInvitesRedemptionCount < ActiveRecord::Migration[6.0]
   def change
-    # TODO FIX
-    # execute <<~SQL
-    #   WITH invite_counts AS (
-    #     SELECT invite_id, COUNT(*) count
-    #     FROM invited_users
-    #     GROUP BY invite_id
-    #   )
-    #   UPDATE invites
-    #   SET redemption_count = GREATEST(redemption_count, count)
-    #   FROM invite_counts
-    #   WHERE invites.id = invite_counts.invite_id
-    # SQL
+    execute <<~SQL
+      WITH invite_counts AS (
+        SELECT invite_id, COUNT(*) count
+        FROM invited_users
+        GROUP BY invite_id
+      )
+      UPDATE invites
+      INNER JOIN invite_counts ON invites.id = invite_counts.invite_id
+      SET redemption_count = GREATEST(redemption_count, count)
+      WHERE invites.id = invite_counts.invite_id
+    SQL
   end
 end
