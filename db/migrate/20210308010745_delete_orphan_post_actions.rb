e22f9ec07e5e678c7c66f8a6fa06f5cdd7c6e848
Hooopo Wang <hoooopo@gmail.com>
2021-10-15 08:43:27 +0800

    fix delete post_actions

--- db/migrate/20210308010745_delete_orphan_post_actions.rb
+++ db/migrate/20210308010745_delete_orphan_post_actions.rb
@@ -2,11 +2,9 @@
 
 class DeleteOrphanPostActions < ActiveRecord::Migration[6.0]
   def up
-    return
-    # TODO FIX
     sql = <<~SQL
-      DELETE FROM post_actions
-      USING post_actions pa
+      DELETE post_actions FROM post_actions
+      INNER JOIN post_actions pa ON post_actions.id = pa.id
       LEFT JOIN posts ON posts.id = pa.post_id
       WHERE posts.id IS NULL
       AND post_actions.id = pa.id
