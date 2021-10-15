00eab8132859426e9c4cc9e582ccaa829098d0a8
Hooopo Wang <hoooopo@gmail.com>
2021-10-16 00:24:11 +0800

    fix add_timestamps_to_optimized_images

--- db/migrate/20200709094846_add_timestamps_to_optimized_images.rb
+++ db/migrate/20200709094846_add_timestamps_to_optimized_images.rb
@@ -6,23 +6,21 @@ class AddTimestampsToOptimizedImages < ActiveRecord::Migration[6.0]
 
     # Start by stealing created/updated at from the uploads table
     # Not perfect, but a good approximation
-    # TODO FIX
-    # execute <<~SQL
-    #   UPDATE optimized_images
-    #   SET created_at = uploads.created_at,
-    #       updated_at = uploads.created_at
-    #   FROM uploads
-    #   WHERE uploads.id = optimized_images.upload_id
-    # SQL
+    execute <<~SQL
+      UPDATE optimized_images
+      JOIN uploads ON uploads.id = optimized_images.upload_id
+      SET optimized_images.created_at = uploads.created_at,
+          optimized_images.updated_at = uploads.created_at
+    SQL
 
-    # # Integrity is not enforced, we might have optimized images
-    # # with no uploads
-    # execute <<~SQL
-    #   UPDATE optimized_images
-    #   SET created_at = NOW(),
-    #       updated_at = NOW()
-    #   WHERE created_at IS NULL
-    # SQL
+    # Integrity is not enforced, we might have optimized images
+    # with no uploads
+    execute <<~SQL
+      UPDATE optimized_images
+      SET created_at = NOW(),
+          updated_at = NOW()
+      WHERE created_at IS NULL
+    SQL
 
     execute <<~SQL
       ALTER TABLE optimized_images modify COLUMN created_at datetime NOT NULL;
