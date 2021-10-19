9423a2b23505469fd60f66369ad7f6fbdd87a64f
Hooopo Wang <hoooopo@gmail.com>
2021-10-19 08:04:47 +0800

    fix topic excerpt length

--- db/post_migrate/20210513125608_remove_length_constrain_from_topic_excerpt.rb
+++ db/post_migrate/20210513125608_remove_length_constrain_from_topic_excerpt.rb
@@ -2,7 +2,7 @@
 
 class RemoveLengthConstrainFromTopicExcerpt < ActiveRecord::Migration[6.1]
   def up
-    change_column :topics, :excerpt, :string, null: true
+    change_column :topics, :excerpt, :text, null: true
   end
 
   def down
