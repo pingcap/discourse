31373eef8c9dd025a956f0a9211fdfb9ef3c8af6
Hooopo Wang <hoooopo@gmail.com>
2021-10-20 11:34:56 +0800

    fix update badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -174,8 +174,8 @@ class Badge < ActiveRecord::Base
   def clear_user_titles!
     DB.exec(<<~SQL, badge_id: self.id, updated_at: Time.zone.now)
       UPDATE users AS u
+      JOIN user_profiles AS up
       SET title = '', updated_at = :updated_at
-      FROM user_profiles AS up
       WHERE up.user_id = u.id AND up.granted_title_badge_id = :badge_id
     SQL
     DB.exec(<<~SQL, badge_id: self.id)
@@ -190,8 +190,8 @@ class Badge < ActiveRecord::Base
   def update_user_titles!(new_title)
     DB.exec(<<~SQL, granted_title_badge_id: self.id, title: new_title, updated_at: Time.zone.now)
       UPDATE users AS u
+      JOIN user_profiles AS up
       SET title = :title, updated_at = :updated_at
-      FROM user_profiles AS up
       WHERE up.user_id = u.id AND up.granted_title_badge_id = :granted_title_badge_id
     SQL
   end
@@ -202,9 +202,9 @@ class Badge < ActiveRecord::Base
   def reset_user_titles!
     DB.exec(<<~SQL, granted_title_badge_id: self.id, updated_at: Time.zone.now)
       UPDATE users AS u
-      SET title = badges.name, updated_at = :updated_at
-      FROM user_profiles AS up
+      JOIN user_profiles AS up
       INNER JOIN badges ON badges.id = up.granted_title_badge_id
+      SET title = badges.name, updated_at = :updated_at
       WHERE up.user_id = u.id AND up.granted_title_badge_id = :granted_title_badge_id
     SQL
   end
